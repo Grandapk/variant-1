adee70f1553785c400e691a24795da35f11983a9
Grandapk <Grandapk@gmail.com>
2025-02-19 02:35:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Пример простой интерактивности
+document.addEventListener("DOMContentLoaded", function () {
+    const heroBtn = document.getElementById("hero-btn");
+  
+    heroBtn.addEventListener("click", function () {
+      alert("Подробнее о наших услугах скоро на сайте!");
+    });
+  });
+  
