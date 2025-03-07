ee891072f13e304567546edd9b2d5b7bc2acbf0d
Grandapk <Grandapk@gmail.com>
2025-03-07 18:19:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Пример простой интерактивности
-document.addEventListener("DOMContentLoaded", function () {
-    const heroBtn = document.getElementById("hero-btn");
-  
-    heroBtn.addEventListener("click", function () {
-      alert("Подробнее о наших услугах скоро на сайте!");
-    });
-  });
-  
